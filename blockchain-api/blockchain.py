686b5761cd8d53f4c48256c4c2a1deb645a21383
Aryan Arora <aryanarora278@gmail.com>
2017-10-28 06:21:07 +0530

    'blah'

--- blockchain-api/blockchain.py
+++ blockchain-api/blockchain.py
@@ -190,11 +190,11 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    parser.add_argument('-p', '--port', default=8000, type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='localhost', port=port)
 
 
 
